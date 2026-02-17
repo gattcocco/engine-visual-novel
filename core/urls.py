00537d53501fe1d144159229fe8c7abbc12feac9
Gattcocco <81433587+gattcocco@users.noreply.github.com>
2026-02-17 16:01:52 +0100

    Implement session-based object-target action flow

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 # IMPORTANTE: Aggiungi 'reset_gioco' alla fine della lista degli import!
-from gameplay.views import motore_gioco, fai_scelta, avvia_action_node, applica_oggetto_action_node, reset_gioco 
+from gameplay.views import motore_gioco, fai_scelta, avvia_action_node, applica_oggetto_action_node, applica_target, reset_gioco 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,6 +16,7 @@ urlpatterns = [
     path('azione/<int:scelta_id>/', fai_scelta, name='fai_scelta'),
     path('action-node/<int:action_node_id>/start/', avvia_action_node, name='avvia_action_node'),
     path('action-node/<int:action_node_id>/apply/<int:oggetto_id>/', applica_oggetto_action_node, name='applica_oggetto_action_node'), 
+    path('<slug:slug_scena>/target/<slug:target_slug>/', applica_target, name='applica_target'),
 
     path('', motore_gioco, name='home'),
     path('<slug:slug_scena>/', motore_gioco, name='scena'),
